79702476c63503691f43c33b2a1e5fe7b65bc54a
zoom <hlin3@leqee.com>
2021-04-21 18:25:53 +0800

    新增iview

--- src/main.js
+++ src/main.js
@@ -3,17 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store/index'
 import ElementUI from 'element-ui'
+import ViewUI from 'view-design';
 
 import formatFn from '@/utils/format'
 import luckyUI from '@/luckyUI/index'
 
 import './styles.scss'
+import 'view-design/dist/styles/iview.css';
 
 import { authList } from '@/config/authConfig'
 
 Vue.use(ElementUI)
   .use(formatFn)
   .use(luckyUI)
+  .use(ViewUI)
 
 Vue.config.productionTip = false
 
--- src/views/forTest/test1.vue
+++ src/views/forTest/test1.vue
@@ -1,67 +1,17 @@
 <template>
   <div class="test">
-    <RegionSelect
-      v-model="value"
-      :options="provinces"
-      @change="handleRegionChange"
-    ></RegionSelect>
+    <LuckyTableBetter
+    ></LuckyTableBetter>
   </div>
 </template>
 
 <script>
-import RegionSelect from './RegionSelect'
+import LuckyTableBetter from '@/luckyUI/components/LuckyTableBetter'
 export default {
-  components: { RegionSelect },
+  components: { LuckyTableBetter },
   props: {},
   data() {
     return {
-      value: [1, 11, 111],
-      provinces: [
-        {
-          name: '浙江省',
-          code: 1,
-          cities: [
-            {
-              name: '杭州市',
-              code: 11,
-              areas: [
-                { name: '西湖区', code: 111 },
-                { name: '余杭区', code: 112 }
-              ]
-            },
-            {
-              name: '宁波市',
-              code: 12,
-              areas: [
-                { name: '江北区', code: 121 },
-                { name: '江东区', code: 122 }
-              ]
-            }
-          ]
-        },
-        {
-          name: '广东省',
-          code: 2,
-          cities: [
-            {
-              name: '广州市',
-              code: 21,
-              areas: [
-                { name: '白云区', code: 211 },
-                { name: '天河区', code: 212 }
-              ]
-            },
-            {
-              name: '深圳市',
-              code: 22,
-              areas: [
-                { name: '宝安区', code: 221 },
-                { name: '南山区', code: 222 }
-              ]
-            }
-          ]
-        }
-      ]
     }
   },
   computed: {},
@@ -69,10 +19,6 @@ export default {
   created() {},
   mounted() {},
   methods: {
-    handleRegionChange(val) {
-      console.log(this.value, 'model')
-      console.log(val, 'change')
-    }
   }
 }
 </script>
